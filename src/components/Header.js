2cacd1af8750aeef82e4af5b0fdd1a73c1cb41bd
suyalthapa09@gmail.com <suyalthapa09@gmail.com>
2025-01-22 14:12:41 +0545

    Main header constructed

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import logo from '../UsedImages/logo.png';
+import '../styles/Header.css';
+
+const Header = () => {
+  const location = useLocation();
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="StreamFusion Logo" className="logo-image" />
+        </Link>
+      </div>
+
+      {location.pathname !== '/login' && location.pathname !== '/register' && location.pathname !== '/upload' && (
+        <div className="search-bar-container">
+          <input
+            type="text"
+            placeholder="Search for a Video...."
+            className="search-input"
+          />
+          
+          <button className="search-icon">🔍
+            {/* <i className="fas fa-search"></i> */}
+          </button>
+        </div>
+      )}
+      <nav className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/upload">Upload</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
