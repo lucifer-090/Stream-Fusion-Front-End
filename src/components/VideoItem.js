7aa6c83f19792a9bdefef65953434e05be084c11
suyalthapa09@gmail.com <suyalthapa09@gmail.com>
2025-01-27 15:52:36 +0545

    front-end updated

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,25 @@
+import '../styles/videoItem.css';
 const VideoItem = ({ video }) => {
     return (
-      <div>
-        <h3>{video.title}</h3>
-        <p>{video.description}</p>
-      </div>
+      // <div>
+      //   <h3>{video.title}</h3>
+      //   <p>{video.description}</p>
+      // </div>
+      <div className="video-list">
+      <h1>Video List</h1>
+      <ul>
+        {video.map((video) => (
+          <li key={video._id}>
+            <h2>{video.title}</h2>
+            <p>{video.description}</p>
+            <video controls width="400">
+              <source src={`http://localhost:9999/${video.path}`} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </li>
+        ))}
+      </ul>
+    </div>
     );
   };
   
